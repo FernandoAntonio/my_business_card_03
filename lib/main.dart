4049f87fcc32e7d8f06bd3cd22e4ca75360ad23a
Fernando Antonio <30207066+fernandoantonio@users.noreply.github.com>
2020-10-29 12:01:37 -0300

    Fixed Mediaquery being called without a sub context

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'constants.dart';
 import 'my_business_card_screen.dart';
 
-void main() => runApp(MyBusinessCardScreen());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: kTeal600),
+      home: MyBusinessCardScreen(),
+    );
+  }
+}
--- lib/my_business_card_screen.dart
+++ lib/my_business_card_screen.dart
@@ -21,65 +21,63 @@ class MyBusinessCardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [kTeal800, kTeal700, kTeal600],
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [kTeal800, kTeal700, kTeal600],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: AssetImage('images/me.jpg'),
+              radius: 50.0,
             ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('images/me.jpg'),
-                radius: 50.0,
+            SizedBox(height: 16.0),
+            Text(
+              'Fernando Antônio',
+              style: TextStyle(
+                fontFamily: 'KaushanScript',
+                color: Colors.white,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(height: 16.0),
-              Text(
-                'Fernando Antônio',
-                style: TextStyle(
-                  fontFamily: 'KaushanScript',
-                  color: Colors.white,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            SizedBox(height: 8.0),
+            GradientText(
+              'FLUTTER DEVELOPER',
+              gradient: LinearGradient(
+                colors: [kTeal300, kTeal200, kTeal100],
               ),
-              SizedBox(height: 8.0),
-              GradientText(
-                'FLUTTER DEVELOPER',
-                gradient: LinearGradient(
-                  colors: [kTeal300, kTeal200, kTeal100],
-                ),
-                style: TextStyle(
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5,
-                ),
+              style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
               ),
-              SizedBox(
-                height: 16.0,
-                width: MediaQuery.of(context).size.width * 0.4,
-                child: Divider(color: kTeal200),
-              ),
-              SizedBox(height: 32.0),
-              MenuCard(
-                title: kMobileNumber,
-                iconData: Icons.phone,
-                onPressed: () => _launchWhatsapp(kMobileNumber),
-              ),
-              SizedBox(height: 16.0),
-              MenuCard(
-                title: kEmail,
-                iconData: Icons.email,
-                onPressed: () => _launchEmail(kEmail),
-              ),
-            ],
-          ),
+            ),
+            SizedBox(
+              height: 16.0,
+              width: MediaQuery.of(context).size.width * 0.4,
+              child: Divider(color: kTeal200),
+            ),
+            SizedBox(height: 32.0),
+            MenuCard(
+              title: kMobileNumber,
+              iconData: Icons.phone,
+              onPressed: () => _launchWhatsapp(kMobileNumber),
+            ),
+            SizedBox(height: 16.0),
+            MenuCard(
+              title: kEmail,
+              iconData: Icons.email,
+              onPressed: () => _launchEmail(kEmail),
+            ),
+          ],
         ),
       ),
     );
